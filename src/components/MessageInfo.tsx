95a6f8dbb4cb1933b71f9894767429524a863e0d
Veroni <zoubheir@gmail.com>
2023-10-07 19:11:45 +0200

    Add message zone

--- src/components/MessageInfo.tsx
+++ src/components/MessageInfo.tsx
@@ -3,7 +3,7 @@ import { faPhone, faVideo } from "@fortawesome/free-solid-svg-icons";
 
 const MessageInfo = () => {
   return (
-    <div className="message-info w-1/6 h-16 rounded-xl absolute top-3 right-2.5 bg-slate-600 flex justify-evenly items-center">
+    <div className="message-info w-1/6 h-16 rounded-xl absolute top-0 right-2.5 bg-slate-600 flex justify-evenly items-center">
       <div className="friend-info bg-yellow-300 w-11 h-11 rounded-full"></div>
       <FontAwesomeIcon icon={faPhone} className="w-8 text-stone-200" />
       <FontAwesomeIcon icon={faVideo} className="w-8 text-stone-200" />
--- src/components/MessageText.tsx
+++ src/components/MessageText.tsx
@@ -1,7 +1,25 @@
+import {
+  faIcons,
+  faMicrophone,
+  faPaperPlane,
+  faPhotoFilm,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const MessageText = () => {
   return (
-    <div className="message-text w-full h-2/6 absolute bottom-0 bg-gray-200 z-10 shadow-separate-bottom">
-      <h1></h1>
+    <div className="message-text w-full h-1/6 absolute bottom-0 bg-gray-200 z-10 shadow-separate-bottom flex justify-center items-center">
+      <div className="text-zone bg-white w-5/6 h-3/6 flex justify-between items-center px-4 rounded-xl">
+        <FontAwesomeIcon icon={faMicrophone} className="w-8 h-8" />
+        <input
+          type="text"
+          placeholder="Type here..."
+          className="w-11/12 h-full outline-none"
+        />
+        <FontAwesomeIcon icon={faPhotoFilm} className="w-6 h-6" />
+        <FontAwesomeIcon icon={faIcons} className="w-6 h-6" />
+      </div>
+      <FontAwesomeIcon icon={faPaperPlane} className="w-6 h-6 mx-5" />
     </div>
   );
 };
